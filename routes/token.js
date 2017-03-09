4e41d5f487eb069d49dc5abc8036a3b2b3512b01
Ryan Thissen <rmt1855@gmail.com>
2017-03-09 14:10:18 -0800

    created part 4 branch, token.js route file

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,21 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex.js');
+const humps = require('humps');
 
 // YOUR CODE HERE
+
+router.get('/books', (req, res) => {
+  knex('books')
+      .orderBy('title')
+      .then((items) => {
+          res.send(humps.camelizeKeys(items));
+      })
+      .catch((err) => {
+          res.sendStatus(500);
+      });
+});
 
 module.exports = router;
