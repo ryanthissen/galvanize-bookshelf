c87e2faefe20da504723504e8b239800b8a7ed32
Ryan Thissen <rmt1855@gmail.com>
2017-03-10 11:46:15 -0800

    added part 5 get routes 2 & 3

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,66 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex.js');
+const humps = require('humps');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt-as-promised');
 
 // YOUR CODE HERE
+
+router.get('/favorites/check', (req, res) => {
+  knex('favorites')
+    .where('book_id', req.query.bookId)
+    .first()
+    .then((favorite) => {
+      if (!favorite) {
+        res.set('Content-Type', 'match/json');
+        res.status(200).send('false');
+      } else {
+        res.set('Content-Type', 'match/json');
+        res.status(200).send('true');
+      }
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
+});
+
+// router.get('/favorites', (req, res) => {
+//
+// })
+
+// router.get('/books/:id', (req, res) => {
+//     knex('books')
+//         .where('id', req.params.id)
+//         .first()
+//         .then((book) => {
+//             if (!book) {
+//                 return next();
+//             }
+//             res.send(humps.camelizeKeys(book));
+//         })
+//         .catch((err) => {
+//             res.sendStatus(500);
+//         });
+// });
+//
+// router.post('/books', (req, res) => {
+//     knex('books')
+//         .insert({
+//             title: req.body.title,
+//             author: req.body.author,
+//             genre: req.body.genre,
+//             description: req.body.description,
+//             cover_url: req.body.coverUrl
+//         }, '*')
+//         .then((items) => {
+//             res.send(humps.camelizeKeys(items[0]));
+//         })
+//         .catch((err) => {
+//             res.sendStatus(500);
+//         });
+// });
 
 module.exports = router;
