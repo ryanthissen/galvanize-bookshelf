50b1d5e9eb9d7ceb8fe2c2a64051331850c5210f
Ryan Thissen <rmt1855@gmail.com>
2017-03-10 14:46:41 -0800

    finished part five, passed all required tests

--- routes/books.js
+++ routes/books.js
@@ -10,99 +10,99 @@ const humps = require('humps');
 // YOUR CODE HERE
 
 router.get('/books', (req, res) => {
-    knex('books')
-        .orderBy('title')
-        .then((items) => {
-            res.send(humps.camelizeKeys(items));
-        })
-        .catch((err) => {
-            res.sendStatus(500);
-        });
+  knex('books')
+    .orderBy('title')
+    .then((items) => {
+      res.send(humps.camelizeKeys(items));
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 });
 
 router.get('/books/:id', (req, res) => {
-    knex('books')
-        .where('id', req.params.id)
-        .first()
-        .then((book) => {
-            if (!book) {
-                return next();
-            }
-            res.send(humps.camelizeKeys(book));
-        })
-        .catch((err) => {
-            res.sendStatus(500);
-        });
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+      res.send(humps.camelizeKeys(book));
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 });
 
 router.post('/books', (req, res) => {
-    knex('books')
-        .insert({
-            title: req.body.title,
-            author: req.body.author,
-            genre: req.body.genre,
-            description: req.body.description,
-            cover_url: req.body.coverUrl
-        }, '*')
-        .then((items) => {
-            res.send(humps.camelizeKeys(items[0]));
-        })
-        .catch((err) => {
-            res.sendStatus(500);
-        });
+  knex('books')
+    .insert({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    }, '*')
+    .then((items) => {
+      res.send(humps.camelizeKeys(items[0]));
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 });
 
 router.patch('/books/:id', (req, res) => {
-    knex('books')
-        .where('id', req.params.id)
-        .first()
-        .then((book) => {
-            if (!book) {
-                return next();
-            }
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
 
-            return knex('books')
-                .update({
-                    title: req.body.title,
-                    author: req.body.author,
-                    genre: req.body.genre,
-                    description: req.body.description,
-                    cover_url: req.body.coverUrl
-                }, '*')
-                .where('id', req.params.id);
-        })
-        .then((items) => {
-            res.send(humps.camelizeKeys(items[0]));
-        })
-        .catch((err) => {
-            res.sendStatus(500);
-        });
+      return knex('books')
+        .update({
+          title: req.body.title,
+          author: req.body.author,
+          genre: req.body.genre,
+          description: req.body.description,
+          cover_url: req.body.coverUrl
+        }, '*')
+        .where('id', req.params.id);
+    })
+    .then((items) => {
+      res.send(humps.camelizeKeys(items[0]));
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 })
 
 router.delete('/books/:id', (req, res) => {
-    let book;
+  let book;
 
-    knex('books')
-        .where('id', req.params.id)
-        .first()
-        .then((row) => {
-            if (!row) {
-                return next();
-            }
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((row) => {
+      if (!row) {
+        return next();
+      }
 
-            book = row;
+      book = row;
 
-            return knex('books')
-                .del()
-                .where('id', req.params.id);
-        })
-        .then(() => {
-            delete book.id;
-            res.send(humps.camelizeKeys(book));
-        })
-        .catch((err) => {
-            res.sendStatus(500);
-        });
+      return knex('books')
+        .del()
+        .where('id', req.params.id);
+    })
+    .then(() => {
+      delete book.id;
+      res.send(humps.camelizeKeys(book));
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 })
 
 module.exports = router;
--- routes/favorites.js
+++ routes/favorites.js
@@ -9,58 +9,96 @@ const bcrypt = require('bcrypt-as-promised');
 
 // YOUR CODE HERE
 
+
 router.get('/favorites/check', (req, res) => {
-  knex('favorites')
-    .where('book_id', req.query.bookId)
-    .first()
-    .then((favorite) => {
-      if (!favorite) {
-        res.set('Content-Type', 'match/json');
-        res.status(200).send('false');
-      } else {
-        res.set('Content-Type', 'match/json');
-        res.status(200).send('true');
-      }
-    })
-    .catch((err) => {
-      res.sendStatus(500);
-    });
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      res.set('Content-Type', 'match/plain');
+      res.status(401).send('Unauthorized');
+    } else {
+      knex('favorites')
+        .where('book_id', req.query.bookId)
+        .first()
+        .then((favorite) => {
+          if (!favorite) {
+            res.set('Content-Type', 'match/json');
+            res.status(200).send('false');
+          } else {
+            res.set('Content-Type', 'match/json');
+            res.status(200).send('true');
+          }
+        })
+        .catch((err) => {
+          res.sendStatus(500);
+        });
+    }
+  });
 });
 
-// router.get('/favorites', (req, res) => {
-//
-// })
+router.get('/favorites', (req, res) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      res.set('Content-Type', 'match/plain');
+      res.status(401).send('Unauthorized');
+    } else {
+      knex.from('favorites').innerJoin('books', 'favorites.book_id', 'books.id')
+        .then((favorites) => {
+          res.send(humps.camelizeKeys(favorites));
+        })
+    }
+  })
+});
 
-// router.get('/books/:id', (req, res) => {
-//     knex('books')
-//         .where('id', req.params.id)
-//         .first()
-//         .then((book) => {
-//             if (!book) {
-//                 return next();
-//             }
-//             res.send(humps.camelizeKeys(book));
-//         })
-//         .catch((err) => {
-//             res.sendStatus(500);
-//         });
-// });
-//
-// router.post('/books', (req, res) => {
-//     knex('books')
-//         .insert({
-//             title: req.body.title,
-//             author: req.body.author,
-//             genre: req.body.genre,
-//             description: req.body.description,
-//             cover_url: req.body.coverUrl
-//         }, '*')
-//         .then((items) => {
-//             res.send(humps.camelizeKeys(items[0]));
-//         })
-//         .catch((err) => {
-//             res.sendStatus(500);
-//         });
-// });
+router.post('/favorites', (req, res) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      res.set('Content-Type', 'match/plain');
+      res.status(401).send('Unauthorized');
+    } else {
+      knex('favorites')
+        .insert({
+          book_id: req.body.bookId,
+          user_id: payload.userId
+        }, '*')
+        .then((favorites) => {
+          const favorite = favorites[0];
+          delete favorite.created_at;
+          delete favorite.updated_at;
+          res.send(humps.camelizeKeys(favorite));
+        });
+    }
+  });
+});
+
+router.delete('/favorites', (req, res) => {
+  let book;
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      res.set('Content-Type', 'match/plain');
+      res.status(401).send('Unauthorized');
+    } else {
+      knex('favorites')
+        .where('book_id', req.body.bookId)
+        .first()
+        .then((row) => {
+          if (!row) {
+            return next();
+          }
+
+          book = row;
+
+          return knex('favorites')
+            .del()
+            .where('book_id', req.body.bookId)
+        })
+        .then(() => {
+          delete book.id,
+            delete book.created_at,
+            delete book.updated_at,
+            res.send(humps.camelizeKeys(book));
+        })
+    }
+  })
+})
 
 module.exports = router;
