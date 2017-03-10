89872b63484d1d952ed3925a099bc0d236877ff4
Ryan Thissen <rmt1855@gmail.com>
2017-03-10 11:11:24 -0800

    added part 5 migrations, seed file

--- migrations/20170309083231_users.js
+++ migrations/20170309083231_users.js
@@ -2,17 +2,17 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', (table) => {
-      table.increments('id').primary();
-      table.string('first_name').notNullable().defaultTo('');
-      table.string('last_name').notNullable().defaultTo('');
-      table.string('email').unique().notNullable();
-      table.specificType('hashed_password', 'char(60)').notNullable();
-      table.timestamps(true, true);
+        table.increments('id').primary();
+        table.string('first_name').notNullable().defaultTo('');
+        table.string('last_name').notNullable().defaultTo('');
+        table.string('email').unique().notNullable();
+        table.specificType('hashed_password', 'char(60)').notNullable();
+        table.timestamps(true, true);
     })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+    return knex.schema.dropTable('users');
 };
 
 
